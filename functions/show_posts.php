4d0b748f58ac58acc9f2dedc8b7933c67d986f63
geronimoD18 <geronimoD18@github.com>
2023-04-25 21:49:18 -0400

    No Posts update

--- functions/show_posts.php
+++ functions/show_posts.php
@@ -50,7 +50,37 @@ if ($result->num_rows > 0) {
     </html>';
   }
 } else {
-  echo "No hay posts disponibles.";
+    echo '
+    <!DOCTYPE html>
+    <html lang="es">
+    <head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Posts - Dev Rat Posts</title>
+    <!-- Importando la versión actual de Bootstrap a la fecha -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-KK94CHFLLe+nY2dmCWGMq91rCGa5gtU4mk92HdvYe+M/SXH301p5ILy+dN9+nJOZ" crossorigin="anonymous">
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ENjdO4Dr2bkBIFxQpeoTz1HIcje39Wm4jDKdf19U8gI4ddQ3GYNS7NTKfAdVQSZe" crossorigin="anonymous"></script>
+    <!-- Importando la versión actual de Font Awesome a la fecha -->
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css" integrity="sha512-iecdLmaskl7CVkqkXNQ/ZH/XLlvWZOJyj7Yy7tcenmpD1ypASozpmT/E0iPtmFIB46ZmdtAc9eNBvH0H/ZpiBw==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+    <!-- Mis fuentes de Google Fonts para el proyecto -->
+    
+    </head>
+    <body>
+    <header>
+        <nav>
+        <!-- Aquí va el menú de navegación -->
+        </nav>
+    </header>
+
+    <main>
+        <span class="text-muted"><strong>No hay publicaciones</strong></span>
+    </main>
+
+    <footer>
+        <!-- Aquí va el contenido del pie de página -->
+    </footer>
+    </body>
+    </html>';
 }
 
 // Cierra la conexión a la base de datos
