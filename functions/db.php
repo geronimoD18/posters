1e4951e74739b53234d477f4509c9359b343acef
geronimoD18 <geronimoD18@github.com>
2023-04-25 20:15:47 -0400

    Updates on the DB file

--- functions/db.php
+++ functions/db.php
@@ -2,4 +2,12 @@
     // Aquí creo la conexión con mi BD local - cambiar valores a lo necesario
     $c = new mysqli('localhost', 'root', '', 'ab_db');
 
+    // Validar conexion con la BD
+    if ($c) {
+        echo "Conexion establecida";
+    } else {
+        echo "Error en la conexion";
+    }
+    
+
 ?>
