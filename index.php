0b8daec4c33b19eb10c5d54efb49b97a6d869be4
geronimoD18 <geronimoD18@github.com>
2023-04-26 02:01:02 -0400

    Fixed bug on these files

--- index.php
+++ index.php
@@ -3,7 +3,7 @@
 <head>
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <title>Dev Rat Posts</title>
+  <title>Dev Rats Posts</title>
   <!-- Importando la versión actual de Bootstrap a la fecha -->
   <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-KK94CHFLLe+nY2dmCWGMq91rCGa5gtU4mk92HdvYe+M/SXH301p5ILy+dN9+nJOZ" crossorigin="anonymous">
   <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ENjdO4Dr2bkBIFxQpeoTz1HIcje39Wm4jDKdf19U8gI4ddQ3GYNS7NTKfAdVQSZe" crossorigin="anonymous"></script>
@@ -18,35 +18,35 @@
   <div class="container main">
 
 <!-- Create bloggo start -->
-<div class="modal fade" id="exampleModalToggle" aria-hidden="true" aria-labelledby="exampleModalToggleLabel" tabindex="-1">
-  <div class="modal-dialog modal-dialog-centered">
-    <div class="modal-content">
-      <div class="modal-header">
-        <h1 class="modal-title fs-5" id="exampleModalToggleLabel">Nueva entrada</h1>
-        <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
-      </div>
-      <div class="modal-body">
-        <form action="functions/submit_post.php" method="post">
-          <label for="title">Título:</label><br>
-          <input type="text" id="title" name="title"><br>
+    <div class="modal fade" id="exampleModalToggle" aria-hidden="true" aria-labelledby="exampleModalToggleLabel" tabindex="-1">
+      <div class="modal-dialog modal-dialog-centered">
+        <div class="modal-content">
+          <div class="modal-header">
+            <h1 class="modal-title fs-5" id="exampleModalToggleLabel">Nueva entrada</h1>
+            <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+          </div>
+          <div class="modal-body">
+            <form action="functions/submit_post.php" method="post">
+              <label for="title">Título:</label><br>
+              <input type="text" id="title" name="title"><br>
 
-          <label for="content">Contenido:</label><br>
-          <textarea id="content" name="content"></textarea><br>
+              <label for="content">Contenido:</label><br>
+              <textarea id="content" name="content"></textarea><br>
 
-          <label for="author">Autor:</label><br>
-          <input type="text" id="author" name="author"><br>
-      </div>
-      <div class="modal-footer">
-          <input type="submit" value="Guardar entrada">
-        </form>
+              <label for="author">Autor:</label><br>
+              <input type="text" id="author" name="author"><br>
+          </div>
+          <div class="modal-footer">
+              <input type="submit" value="Guardar entrada">
+            </form>
+          </div>
+        </div>
       </div>
     </div>
-  </div>
-</div>
 
-<button class="btn btn-primary" data-bs-target="#exampleModalToggle" data-bs-toggle="modal">Crear nuevo blog</button>
+    <button class="btn btn-primary" data-bs-target="#exampleModalToggle" data-bs-toggle="modal">Crear nuevo blog</button>
 <!-- Create bloggo end -->
-<hr>
+    <hr>
 <!-- Print bloggos start -->
 
     <div class="blogs">
@@ -65,10 +65,10 @@
                 echo '
                     <div>
                         <h4>'. $row["title"] .'</h4>
-                        <span class="text-muted">Publicado por <strong><em>'. $row["author"] .'</en></strong> el <strong><em>'. date('d \d\e F, Y', strtotime($row["date"])) .'</en></strong></span><br>
+                        <span class="text-muted">Publicado por <strong><em>'. $row["author"] .'</em></strong> el <strong><em>'. date('d \d\e F, Y', strtotime($row["date"])) .'</em></strong></span><br>
                         <p>'. $row["content"] .'</p>
                         <center><span>...</span></center>
-                    </div><br>';
+                    </div>';
             }
         } else {
             echo '
